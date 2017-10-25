cf0a397b99b93739f6aec44b04c23f84db189492
Basit Parkar <basit.parkar@careem.com>
2017-10-25 09:28:52 +0400

    Bleh..

--- app/src/main/java/me/iz/power4u/DashboardActivity.java
+++ app/src/main/java/me/iz/power4u/DashboardActivity.java
@@ -3,6 +3,7 @@ package me.iz.power4u;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,28 +13,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+  private TextView tvText;
+  private ImageView ivImg;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_dashboard);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
 
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle =
         new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
     drawer.addDrawerListener(toggle);
     toggle.syncState();
 
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    tvText = findViewById(R.id.tvText);
+    ivImg = findViewById(R.id.ivImg);
     navigationView.setNavigationItemSelectedListener(this);
   }
 
   @Override public void onBackPressed() {
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    DrawerLayout drawer = findViewById(R.id.drawer_layout);
     if (drawer.isDrawerOpen(GravityCompat.START)) {
       drawer.closeDrawer(GravityCompat.START);
     } else {
@@ -66,12 +74,21 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
     int id = item.getItemId();
 
     if (id == R.id.nav_power_check) {
-      // Handle the camera action
-    } else if (id == R.id.nav_smart_advice) {
+      ivImg.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_bar_chart));
+      tvText.setText("Some text here");
 
+    } else if (id == R.id.nav_smart_advice) {
+      ivImg.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_homeauto));
+      tvText.setText(getString(R.string.smart_advice));
+
+      // Put images in drawable folder and use.
     } else if (id == R.id.nav_smart_saver) {
+      ivImg.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_bar_chart));
+      tvText.setText(getString(R.string.smart_saver_mode));
 
     } else if (id == R.id.nav_house_control) {
+      ivImg.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_bar_chart));
+      tvText.setText("something..");
 
     }
 
